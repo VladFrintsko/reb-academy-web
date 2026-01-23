5419d9d28e28fe65c7ce5c0e6876a38b849d32f3
Vladyslav.Frintsko <frintskov14@gmail.com>
2026-01-23 12:52:20 +0200

    fix tailwind styles

--- src/pages/docs/components/DocsTopbar.tsx
+++ src/pages/docs/components/DocsTopbar.tsx
@@ -1,14 +1,12 @@
 export function DocsTopbar() {
     return (
-        <header className="border-b p-4">
-            <div className="">
+        <header className="border-b">
+            <div className="mx-auto flex w-full max-w-[1400px] items-center justify-between gap-6 px-6 py-1">
                 <div>REB ACADEMY</div>
 
-                <div>
-                    <input
-                        placeholder="Search"
-                    />
-                    <button>
+                <div className="flex items-center gap-2">
+                    <input placeholder="Search" className="rounded-md border border-white/10 bg-white/5 px-3 py-2 text-sm outline-none" />
+                    <button className="rounded-md bg-white/10 px-10 py-5 text-sm">
                         Search ✨
                     </button>
                 </div>
